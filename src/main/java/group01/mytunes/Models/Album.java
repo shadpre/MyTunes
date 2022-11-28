41a1e0f579dfe7b79bcf6f7314ab1fcb81e362f0
Klavs Tranborg <klavs@tranborg.eu>
2022-11-28 12:33:24 +0100

    Models added

---
+++ src/main/java/group01/mytunes/Models/Album.java
@@ -0,0 +1,37 @@
+package group01.mytunes.Models;
+
+public class Album {
+    private int id;
+    private String name;
+    private int artistId;
+
+    public Album(int id, String name, int artistId) {
+        this.id = id;
+        this.name = name;
+        this.artistId = artistId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
+}
---
+++ src/main/java/group01/mytunes/Models/Artist.java
@@ -0,0 +1,27 @@
+package group01.mytunes.Models;
+
+public class Artist {
+    private int id;
+    private String Name;
+
+    public Artist(int id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
