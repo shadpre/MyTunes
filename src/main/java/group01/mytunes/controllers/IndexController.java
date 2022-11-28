48eb13eefd5033cbef7c1d44157ec9414f630f1a
TheDarkSide1992 <Jenscp171@gmail.com>
2022-11-28 13:13:04 +0100

    GUI V1.2

--- src/main/java/group01/mytunes/controllers/IndexController.java
+++ src/main/java/group01/mytunes/controllers/IndexController.java
@@ -1,13 +1,37 @@
 package group01.mytunes.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class IndexController implements Initializable {
+    @FXML private Button btnPlaylistEdit;
+    @FXML private Button btnPlaylistNew;
+    @FXML private Button btnPlaylistDelete;
+    @FXML private Button btnPlaylistUp;
+    @FXML private Button btnPlaylistDown;
+    @FXML private Button btnSongEdit;
+    @FXML private Button btnSongNew;
+    @FXML private Button btnSongDelete;
+    @FXML private Button btnClose;
+    @FXML private Button btnMoveSongToPlaylist;
+    @FXML private Button btnSearch;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("Controller initialized");
+    }
+
+    private void displayError(Throwable t)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("!!ERROR!!");
+        alert.setHeaderText("Something went wrong, \n ERROR:      " + t.getMessage());
+        alert.showAndWait();
     }
 }
