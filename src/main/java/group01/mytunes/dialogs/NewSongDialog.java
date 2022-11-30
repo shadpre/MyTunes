e07f335bbf48b8bc1eac72a79e10f7557d6c46ad
Lasse Emil Hansen <leh99dk.mail@gmail.com>
2022-11-30 10:58:39 +0100

    Can now pick artist and album.

--- src/main/java/group01/mytunes/dialogs/NewSongDialog.java
+++ src/main/java/group01/mytunes/dialogs/NewSongDialog.java
@@ -1,11 +1,15 @@
 package group01.mytunes.dialogs;
 
+import group01.mytunes.Models.Album;
 import group01.mytunes.Models.Artist;
 import group01.mytunes.Models.Song;
+import group01.mytunes.dao.AlbumDatabaseDAO;
 import group01.mytunes.dao.ArtistDatabaseDAO;
+import group01.mytunes.dao.interfaces.IAlbumDAO;
 import group01.mytunes.dao.interfaces.IArtistDAO;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
@@ -15,69 +19,76 @@ import javafx.stage.Window;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-
 public class NewSongDialog extends Dialog<Song> {
 
-        private File selectedSongFile;
+    private File selectedSongFile;
 
-        @FXML
-        private ComboBox<Artist> artistPicker;
+    @FXML
+    private ComboBox<Artist> artistPicker;
 
-        @FXML
-        private Button filePickerButton;
+    @FXML
+    private ComboBox<Album> albumPicker;
 
-        @FXML
-        private TextField titleTextField;
+    @FXML
+    private Button filePickerButton;
 
-        private IArtistDAO artistDAO = new ArtistDatabaseDAO();
+    @FXML
+    private TextField titleTextField;
 
-        public NewSongDialog(Window owner) {
-            try {
-                FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(getClass().getResource("../AddSongDialog.fxml"));
-                loader.setController(this);
+    private IArtistDAO artistDAO;
+    private IAlbumDAO albumDAO;
+    private ObservableList<Artist> artistObservableList;
+    private ObservableList<Album> albumObservableList;
 
-                DialogPane pane = loader.load();
+    public NewSongDialog(Window owner) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("../AddSongDialog.fxml"));
+            loader.setController(this);
 
-                filePickerButton.setOnAction(event -> {
-                    FileChooser fileChooser = new FileChooser();
-                    fileChooser.setTitle("Select song");
-                    fileChooser.getExtensionFilters().add(
-                            new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"));
-                    File selectedFile = fileChooser.showOpenDialog(owner);
+            DialogPane pane = loader.load();
 
-                    if (selectedFile != null) {
-                        selectedSongFile = selectedFile;
-                        System.out.println(selectedFile.getAbsoluteFile());
-                    }
-                });
+            filePickerButton.setOnAction(event -> {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Select song");
+                fileChooser.getExtensionFilters().add(
+                        new FileChooser.ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"));
+                File selectedFile = fileChooser.showOpenDialog(owner);
 
-                List<Artist> artists = artistDAO.getArtists();
-                artistPicker.setItems(FXCollections.observableArrayList(artists));
+                if (selectedFile != null) {
+                    selectedSongFile = selectedFile;
+                    System.out.println(selectedFile.getAbsoluteFile());
+                }
+            });
 
-                initOwner(owner);
-                initModality(Modality.APPLICATION_MODAL);
+            artistDAO = new ArtistDatabaseDAO();
+            albumDAO = new AlbumDatabaseDAO();
 
-                setResizable(false);
-                setTitle("Add song");
-                setDialogPane(pane);
+            artistObservableList = FXCollections.observableArrayList(artistDAO.getArtists());
+            albumObservableList = FXCollections.observableArrayList(albumDAO.getAlbums());
 
-                setResultConverter(buttonType -> {
-                    if(selectedSongFile == null) return null;
-                    try {
-                        byte[] fileData = Files.readAllBytes(selectedSongFile.toPath());
-                        return new Song( 1, titleTextField.getText(), fileData, 0);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+            artistPicker.setItems(artistObservableList);
+            albumPicker.setItems(albumObservableList);
 
-                setOnShowing(dialogEvent -> Platform.runLater(() -> titleTextField.requestFocus()));
+            initOwner(owner);
+            initModality(Modality.APPLICATION_MODAL);
 
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
+            setResizable(false);
+            setTitle("Add song");
+            setDialogPane(pane);
+
+            setResultConverter(buttonType -> {
+                if(selectedSongFile == null) return null;
+                if(titleTextField.getText().isEmpty()) return null;
+                if(artistPicker.getItems().isEmpty()) return null;
+
+                return new Song( 1, titleTextField.getText(), new byte[] {(byte) 0x0}, 0);
+            });
+
+            setOnShowing(dialogEvent -> Platform.runLater(() -> titleTextField.requestFocus()));
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
+    }
 }
